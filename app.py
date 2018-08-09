5a6cc3f21b301372c1e19d4089d5cc7a441df358
Lucter <42231816+Lucter@users.noreply.github.com>
2018-08-09 14:00:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,28 +35,6 @@ def add_numbers_post():
   	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
 
 
-@app.route('/shopping_list', methods=['GET','POST'])
-def shopping_list_post():
-	  # --> ['5', '6', '8']
-	  # print(type(request.form['text']))
-
-    if request.method == 'GET':
-      return render_template('shopping_list.html')
-    elif request.method == 'POST':
-	print request.form['text'].split()
-                 
-          shop_list = []
-          try:
-            for item in request.form['text'].split():
-              
-              shop_list.append(item)
-
-              
-              
-            return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
-          except ValueError:
-            return "Easy now! Let's keep it simple! Just words with a space between them"
-          
   	      
 @app.route('/time', methods=['GET','POST'])
 def time_post():
